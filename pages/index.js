f3657470436e8624af34d94a206256b269d2f25d
IvanSmiths <info@ivansmiths.com>
2021-01-14 21:08:23 +0100

    MOD OG Test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,17 @@ export default function Home() {
         <meta name="theme-color" content="#fdf033" />
         <meta name="msapplication-navbutton-color" content="#fdf033" />
         <meta name="apple-mobile-web-app-status-bar-style" content="#fdf033" />
-        <meta property="og:url" content="http://www.holycannoli.xyz" />
+        <meta
+          property="og:url"
+          content="http://www.holycannoli.xyz https://i.ibb.co/XZ9tTrx/og.jpg"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://i.ibb.co/XZ9tTrx/og.jpg"
+        />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="1400" />
+        <meta property="og:image:height" content="608" />
         <meta property="og:title" content="Holy Cannoli." />
         <meta property="og:locale" content="it_IT" />
         <meta property="og:description" content="I cannoli invadono Ragusa!" />
