0f7bfa9b16974e50a617bf6c0f924c8402b34390
IvanSmiths <info@ivansmiths.com>
2021-01-14 20:56:12 +0100

    ADD PWA & Open Graph

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
-import dynamic from 'next/dynamic';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 import Head from 'next/head';
-
-const Model = dynamic(() => import('../components/Model'), { ssr: false });
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Un Cannolo.</title>
+        <title>Holy Cannoli!</title>
+        <meta name="description" content="I cannoli 3D invadono Ragusa!" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#fdf033" />
+        <meta name="msapplication-navbutton-color" content="#fdf033" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#fdf033" />
+        <meta property="og:url" content="http://www.holycannoli.xyz" />
+        <meta property="og:title" content="Holy Cannoli." />
+        <meta property="og:locale" content="it_IT" />
+        <meta property="og:description" content="I cannoli invadono Ragusa!" />
+        <meta
+          property="og:image"
+          content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+        />
       </Head>
       <Main />
       <Footer />
