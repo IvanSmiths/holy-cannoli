534592dc457fa9cd4f6d71b9121a583bafaf0648
IvanSmiths <info@ivansmiths.com>
2021-01-15 20:29:33 +0100

    ADD Cannolo 3D

--- components/Model.js
+++ components/Model.js
@@ -7,9 +7,11 @@ const Model = () => {
         id="reveal"
         class="model"
         src="/3d-model.glb"
-        ios-src=""
+        poster="/poster.png"
+        ios-src="/3d-model.usdz"
         ar
         camera-controls
+        camera-orbit="-150.8deg 77.13deg 2.982m"
         auto-rotate
         auto-rotate-delay="500"
         alt="Modello 3D di un Cannolo"
